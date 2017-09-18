afc8bc5b4c50d432baa186c7092dd927150d9f17
Peter <kuimeli2@illinoi.edu>
2017-09-18 00:19:17 -0500

    added hyperparameter search

--- pytorch_char_rnn/helpers.py
+++ pytorch_char_rnn/helpers.py
@@ -6,6 +6,16 @@ import random
 import time
 import math
 import torch
+import numpy as np
+
+# Random hyperparamter configuration
+
+def generate_random_hyperparams(lr_min, lr_max, K_min, K_max, num_layers_min, num_layers_max):
+    lr_exp = np.random.uniform(lr_min, lr_max)
+    lr = 10**(lr_exp)
+    K = np.random.choice(np.arange(K_min, K_max+1),1)[0]
+    num_layers = np.random.choice(np.arange(num_layers_min, num_layers_max + 1),1)[0]
+    return lr, K, num_layers
 
 # Reading and un-unicode-encoding data
 
